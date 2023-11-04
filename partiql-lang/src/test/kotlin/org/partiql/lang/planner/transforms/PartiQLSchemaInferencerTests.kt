e65bcb80f50074b43b533e669aaa942d55a48e84
Yingtao Liu <yliuuu@amazon.com>
2023-11-03 21:36:14 -0700

    change loader logic

--- partiql-lang/src/test/kotlin/org/partiql/lang/planner/transforms/PartiQLSchemaInferencerTests.kt
+++ partiql-lang/src/test/kotlin/org/partiql/lang/planner/transforms/PartiQLSchemaInferencerTests.kt
@@ -54,6 +54,7 @@ import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 class PartiQLSchemaInferencerTests {
+    private val provider = PartiQLTestProvider()
 
     init {
         // load test inputs
@@ -135,9 +136,7 @@ class PartiQLSchemaInferencerTests {
     fun testSubqueries(tc: TestCase) = runTest(tc)
 
     companion object {
-        private val provider = PartiQLTestProvider
-
-        private val root = PartiQLTestProvider.javaClass.getResource("/catalogs/default")!!.toURI().toPath().pathString
+        private val root = PartiQLTestProvider().javaClass.getResource("/catalogs/default")!!.toURI().toPath().pathString
 
         private val PLUGINS = listOf(LocalPlugin())
 
